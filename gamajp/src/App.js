d73c81db668cf00819032d20cf732ec817967f07
João Paulo <joaopaulokimi@gmail.com>
2021-07-13 20:44:52 -0300

    methods, axios,requests

--- gamajp/src/App.js
+++ gamajp/src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App(props) {
 
   const [usuario, setUsu] = useState('');
+  console.log(usuario)
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
+  }
 
   return (
     <>
     <h1>{ props.title } { props.user }</h1>
     <h1>{usuario}</h1>
     <input className='usuarioInput' placeholder='Nome' onChange={e => setUsu(e.target.value)}/>
-    <button type='button'>Pesquisa</button>
+    <button type='button' onClick={handlePesquisa}>Pesquisa</button>
     </>
   );
 }
