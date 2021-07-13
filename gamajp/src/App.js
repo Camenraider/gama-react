63fbdffb765fac08ba3cd339ab83275e14479a80
João Paulo <joaopaulokimi@gmail.com>
2021-07-13 20:22:41 -0300

    estados, componetes, cookies, fragment

--- gamajp/src/App.js
+++ gamajp/src/App.js
@@ -1,24 +1,18 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [usuario, setUsu] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>{ props.title } { props.user }</h1>
+    <h1>{usuario}</h1>
+    <input className='usuarioInput' placeholder='Nome' onChange={e => setUsu(e.target.value)}/>
+    <button type='button'>Pesquisa</button>
+    </>
   );
 }
 
