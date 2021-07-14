32f82742c7ab43ab69977e70350660eba40a6128
João Paulo <joaopaulokimi@gmail.com>
2021-07-13 21:21:05 -0300

    incorporando as rotas

--- gamajp/src/App.js
+++ gamajp/src/App.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
+import Routes from './routers';
 
-function App(props) {
 
-  const [usuario, setUsu] = useState('');
-  console.log(usuario)
-
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
-  }
-
+function App(){
   return (
     <>
-    <h1>{ props.title } { props.user }</h1>
-    <h1>{usuario}</h1>
-    <input className='usuarioInput' placeholder='Nome' onChange={e => setUsu(e.target.value)}/>
-    <button type='button' onClick={handlePesquisa}>Pesquisa</button>
+
     </>
   );
 }
